9ead7d5ad199963a9ab631b49d82fb61c003865f
mahaeswari <mahaeswariarumugam@gmail.com>
2018-05-29 18:59:18 +0530

    LoopingProgramExample

---
+++ src/com/basic/LoopingProgramExample.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class LoopingProgramExample {
+	public static void main(String a[])
+	{
+		int x=1,total=0;
+		while(x<=10)
+		{
+			total+=x;
+			++x;
+			System.out.println(x);
+		}
+	}
+
+}
