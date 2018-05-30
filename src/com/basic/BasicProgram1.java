61c9f63dab5eebdceaf36b5df5fd1dd40d9b2015
mahaeswari <mahaeswariarumugam@gmail.com>
2018-05-30 10:42:36 +0530

    BasicProgram1

---
+++ src/com/basic/BasicProgram1.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class BasicProgram1 {
+	public static void main(String []args)
+	{
+		int y=0;
+		while(y>0)
+			System.out.println("y= " + y);
+		++y;
+		System.out.println(y);
+	}
+
+}
